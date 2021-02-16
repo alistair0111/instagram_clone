c203adbad147db892cabd2cc96805c6ac7dd906b
Alistair Baretto <alistair.baretto@gmail.com>
2021-02-17 00:12:58 +0530

    Image Saving In Firestore

--- instagram/components/main/Add.js
+++ instagram/components/main/Add.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View, Button, Image } from 'react-native';
 import { Camera } from 'expo-camera';
 import * as ImagePicker from 'expo-image-picker';
 
-export default function Add() {
+export default function Add({ navigation }) {
   const [hasGalleryPermission, setHasGalleryPermission] = useState(null);
   const [hasCameraPermission, setHasCameraPermission] = useState(null);
 
@@ -74,8 +74,12 @@ export default function Add() {
             onPress={() => takePicture()}
         />
         <Button
-            title='pick Image From gallery'
+            title='Pick Image From gallery'
             onPress={() => pickImage()}
+        />
+        <Button
+            title='Save'
+            onPress={() => navigation.navigate('Save', {image})}
         />
         {image && <Image source={{ uri: image}} style={{flex: 1}}/>}
     </View>
