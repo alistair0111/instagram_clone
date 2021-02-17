246f19b62a75108499036fdb87590ab430499401
Alistair Baretto <alistair.baretto@gmail.com>
2021-02-17 11:19:13 +0530

    UserPosts in profile

--- instagram/components/Main.js
+++ instagram/components/Main.js
@@ -4,7 +4,7 @@ import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityI
 
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux'; 
-import { fetchUser } from '../redux/actions/index';
+import { fetchUser, fetchUserPosts } from '../redux/actions/index';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 
 import FeedScreen from './main/Feed';
@@ -19,6 +19,7 @@ const Tab = createMaterialBottomTabNavigator();
 export class Main extends Component {
     componentDidMount(){
         this.props.fetchUser();
+        this.props.fetchUserPosts();
     }
     render() {
         return (
@@ -56,6 +57,6 @@ const mapStateToProps = (store) => ({
     currentUser: store.userState.currentUser
 })
 
-const mapDispatchToProps = (dispatch) => bindActionCreators({fetchUser}, dispatch)
+const mapDispatchToProps = (dispatch) => bindActionCreators({ fetchUser, fetchUserPosts}, dispatch)
 
 export default connect(mapStateToProps, mapDispatchToProps)(Main)
